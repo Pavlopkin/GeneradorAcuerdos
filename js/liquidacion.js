b5aef956786fffdef9672bf7ba2202d343b51c42
Pavlopkin <90423128+Pavlopkin@users.noreply.github.com>
2023-02-15 11:32:46 -0300

    Update liquidacion.js

--- js/liquidacion.js
+++ js/liquidacion.js
@@ -253,10 +253,10 @@ $("#formulario").submit(function (e) {
     $("#juicio").append(`${juicio}`);
     let monto = formulario.gastosJuicio.children[2].value;
     if(document.getElementById('radio1').checked){
-        gastos = 3427;
+        gastos = 3553;
     }else
     if(document.getElementById('radio2').checked){
-        gastos = 6854;
+        gastos = 7107;
     }
     if(document.getElementById('radio3').checked){
         let cantidad = document.getElementById('selectorServicios').value;
@@ -388,8 +388,8 @@ function liquidacion(a){
     let tasa = a.monto * 0.022;
     let sTasa = tasa * 0.1;
     let honorarios = (a.monto * a.porcentaje)/100;
-    if(honorarios < 20562){
-        honorarios = 20562;
+    if(honorarios < 21321){
+        honorarios = 21321;
     }
     let aportes = honorarios *0.1;
     class resultado {
